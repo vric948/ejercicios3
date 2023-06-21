584f68795d0c58163c2677e6268ca234e644637b
vric948 <vric948@hotmail.com>
2023-06-20 23:11:49 -0700

    Ejercicios dia 2

---
+++ eclipse-workspace/ejercicios_FUNVAL/src/ejercicios_FUNVAL/Ejercicios2/Ejercicio2_2.java
@@ -0,0 +1,37 @@
+package ejercicios_FUNVAL.Ejercicios2;
+
+import java.util.Scanner;
+
+public class Ejercicio2_2 {
+	
+	public String nombre;
+	public byte edad;
+	
+	public void ingresarDatos() {
+		try (Scanner input = new Scanner(System.in)) {
+			System.out.println("Ingrese nombre");
+			nombre = input.nextLine();
+			
+			System.out.println("Ingrese edad");
+			edad = input.nextByte();
+		}
+	}
+	
+	public void imprimirDatos() {
+		System.out.println("Sus datos son los siguientes" + System.lineSeparator() + "Nombre: " + nombre + System.lineSeparator() + "Edad: " + edad);
+		
+	}
+	
+	public void saludo() {
+		System.out.println("Bienvenido/a " + nombre);
+	}
+	public static void main(String[] args) {
+		Ejercicio2_2 persona = new Ejercicio2_2();
+				
+		persona.ingresarDatos();
+        persona.imprimirDatos();
+        persona.saludo();
+
+	}
+
+}
